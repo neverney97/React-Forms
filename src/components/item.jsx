b1f8a83a3d1863e9072c5e37b3c3d8751a229ef8
Emmanuel <nanie43@yahoo.com>
2025-06-04 20:06:48 +0000

    added heading

--- src/components/item.jsx
+++ src/components/item.jsx
@@ -2,13 +2,15 @@ import React from 'react'
 
 const Item = ({details}) => {
   return (
-    <div className='flex items-center justify-between bg-white rounded-xl shadow-sm hover:shadow-md p-4 mb-4 transition-transform transform hover:scale-105'>
-        <h1 className='text-xl font-semibold text-slate-800'>
-            {details.name}
-        </h1>
-        <h3>
-            {details.email}
-        </h3>
+    <div >
+        <div className='flex items-center justify-between bg-white rounded-xl shadow-sm hover:shadow-md p-4 mb-4 transition-transform transform hover:scale-105'>
+            <h1 className='text-xl font-semibold text-slate-800'>
+                {details.name}
+            </h1>
+            <h3>
+                {details.email}
+            </h3>
+        </div>
     </div>
   )
 }
